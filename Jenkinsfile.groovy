e7645d9b6202e8baafb6139b908ea459de0cc4ce
kolomyya <andriyzn4@gmail.com>
2019-03-24 15:51:41 -0500

    Jenkinsfile created

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+node {
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please specify IP', name: 'IP', trim: true)])])
+    satge ("Remove container") {
+        try {
+            sh "ssh root${IP} docker rm  -f flaskex"
+    }
+        catch(exc) {
+            sh "echo container deleted"
+        }
+    }
+       
+    stage("Run container " {
+        sh " ssh root${@IP}  docker run -d  --name Flaskex -p 6000:3000  kolomyya/flaskex:v2" )
+    }    
+}
