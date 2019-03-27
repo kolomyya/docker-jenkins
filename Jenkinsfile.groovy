21fe75bd6c935be4fda9f6a49d43378a2dc6bed1
kolomyya <44345671+kolomyya@users.noreply.github.com>
2019-03-27 16:49:57 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 node {
- properties([parameters([string(defaultValue: '159.203.122.51', description: 'Docker Host', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'Version of the App', name: 'VER', trim: false), string(defaultValue: '4000', description: 'What port would you like to run', name: 'PORT', trim: false)])])    satge ("Remove container") {
+ properties([parameters([string(defaultValue: '159.203.122.51', description: 'Docker Host', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'Version of the App', name: 'VER', trim: false), string(defaultValue: '4000', description: 'What port would you like to run', name: 'PORT', trim: false)])])    
+    stage ("Remove container") {
         try {
             sh "ssh root@${IP} docker rm  -f flaskex"
     }
