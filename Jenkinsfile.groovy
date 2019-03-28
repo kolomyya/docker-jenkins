3f9c1cd9d5cdc9e80029ed5f37ed258f12a6d12d
kolomyya <44345671+kolomyya@users.noreply.github.com>
2019-03-28 12:54:27 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,5 @@
 node {
  properties([parameters([string(defaultValue: '159.203.122.51', description: 'Docker Host', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'Version of the App', name: 'VER', trim: false), string(defaultValue: '4000', description: 'What port would you like to run', name: 'PORT', trim: false)])])    
-    stage ("Remove container") {
-        try {
-            sh "ssh root@${IP} docker rm  -f flaskex"
-    }
-        catch(exc) {
-            sh "echo container deleted"
-        }
-    }
-
     stage("Run container ") {
         sh " ssh root@${IP}  docker run -d  --name Flaskex -p ${PORT}  kolomyya/flaskex:${VER}" 
      }    
